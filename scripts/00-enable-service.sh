467417fde8314ba013dddb5450764714fd1179af
MGUER04 <michele.guerriero@vodafone.com>
2020-09-16 15:02:11 +0200

    Uncomment 00-enable-service.sh

--- scripts/00-enable-service.sh
+++ scripts/00-enable-service.sh
@@ -1,5 +1,3 @@
 gcloud services enable \
     dataproc.googleapis.com \
-    pubsub.googleapis.com #\
-#    cloudfunctions.googleapis.com \
-#    datastore.googleapis.com
+    pubsub.googleapis.com
--- scripts/09-configure-ingestion-vm.sh
+++ scripts/09-configure-ingestion-vm.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
-sudo apt-get install -y python3-pip
+sudo apt-get install -y python3-pip git
 pip3 install pytz google-cloud-storage google-cloud-pubsub
